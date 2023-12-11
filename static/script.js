7c42ae4e45f5b09804b23b264ba5d06701306153
TechniKalCancer <Techminer49er@gmail.com>
2023-12-11 12:53:50 -0500

    Update script.js
    More better.

--- static/script.js
+++ static/script.js
@@ -14,8 +14,13 @@ function updateHistoryTable(data) {
         const checkoutCell = document.createElement('td');
 
         assetCell.textContent = item.asset;
-        checkinCell.textContent = item.check_in || '-';
-        checkoutCell.textContent = item.check_out || '-';
+
+        // Check if 'checkin' or 'checkout' events exist in the history
+        const checkinEvent = item.history.find(event => event.action === 'checkin');
+        const checkoutEvent = item.history.find(event => event.action === 'checkout');
+
+        checkinCell.textContent = checkinEvent ? checkinEvent.timestamp : '-';
+        checkoutCell.textContent = checkoutEvent ? checkoutEvent.timestamp : '-';
 
         row.appendChild(assetCell);
         row.appendChild(checkinCell);
@@ -41,24 +46,17 @@ assetInput.addEventListener('change', () => {
 // Event listener for "Enter" key press on the input field
 assetInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-        // Toggle between Check In and Check Out
-        currentAction = currentAction === 'checkin' ? 'checkout' : 'checkin';
-        toggleBtn.textContent = currentAction === 'checkin' ? 'Check In' : 'Check Out';
-
         // Prevent the default behavior (form submission)
         event.preventDefault();
-
-        // Clear the input field
-        assetInput.value = '';
-
         // Make API call to update check-in/check-out status
         updateAssetStatus(assetInput.value.trim(), currentAction);
+        // Clear the input field
+        assetInput.value = '';
     }
 });
 
 // Event listener for toggle button
 toggleBtn.addEventListener('click', () => {
-    // Toggle between Check In and Check Out
     currentAction = currentAction === 'checkin' ? 'checkout' : 'checkin';
     toggleBtn.textContent = currentAction === 'checkin' ? 'Check In' : 'Check Out';
 });
@@ -69,7 +67,7 @@ function updateAssetStatus(assetId, action) {
     console.log('Asset ID:', assetId);
 
     // Make API call to update check-in/check-out status
-    fetch(`/api/assets/${assetId}`, {  // Ensure assetId is included in the URL
+    fetch(`/api/assets/${assetId}`, {
         method: 'POST',
         body: JSON.stringify({ action }),
         headers: {
@@ -85,6 +83,10 @@ function updateAssetStatus(assetId, action) {
     })
     .then(data => {
         console.log('Data received from server:', data);
+
+        // Display notification
+        showNotification(`Asset ${data.asset.asset} updated successfully`, 'green', 5000);
+
         // Fetch and update asset history
         fetch('/api/assets')
             .then(response => response.json())
@@ -92,7 +94,28 @@ function updateAssetStatus(assetId, action) {
     })
     .catch(error => {
         console.error('Error updating asset status:', error);
+        // Display error notification
+        showNotification('Error updating asset status', 'red', 5000);
     });
+}
+
+// Function to display notification
+function showNotification(message, color, timeout) {
+    const notification = document.createElement('div');
+    notification.style.backgroundColor = color;
+    notification.style.color = 'white';
+    notification.style.padding = '10px';
+    notification.style.borderRadius = '5px';
+    notification.style.position = 'fixed';
+    notification.style.top = '10px';
+    notification.style.right = '10px';
+    notification.textContent = message;
+    document.body.appendChild(notification);
+
+    // Automatically hide after a timeout
+    setTimeout(() => {
+        document.body.removeChild(notification);
+    }, timeout);
 }
 
 // Fetch and display initial asset history
